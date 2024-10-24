93909f0c18a1948bc755620cf71b82e459b4b1ef
Amina Wasif <aminawasif20@gmail.com>
2024-10-24 12:28:46 +0500

    Set up Tailwind CSS with this Vite project

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 // rafce -> react arrow function component export (ES7+ Extension Shortcut)
 // rafc -> react arrow function component (ES7+ Extension Shortcut)
 
+// <></> is a fragment. (Doesn't show up in DOM)
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <div className="text-5xl">App</div>
+      <p>Hello</p>
+    </>
+  );
 };
 
 export default App;
