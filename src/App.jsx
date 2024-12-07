19b5e6e0a461c8a3f96495429f7fdaf2fa1a1935
Amina Wasif <77668986+AW-2021@users.noreply.github.com>
2024-12-08 00:14:35 +0500

    Modified App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+// import React from 'react';
 
 const App = () => {
   return (
@@ -76,29 +76,31 @@ const App = () => {
      <section className='bg-blue-50 px-4 py-10'>
       <div className='container-xl lg:container m-auto'>
         <h2 className='text-3xl font-bold text-center text-indigo-500 mb-6'>Browse Jobs</h2>
-        <div>
+        <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
           {/* Job Listing 1 */}
-          <div>
-            <div>
-              <div>
-                <div>Full-Time</div>
-                <h3>Senior React Developer</h3>
+          <div className='bg-white rounded-xl shadow-md relative'>
+            <div className='p-4'>
+              <div className='mb-6'>
+                <div className='text-gray-600 my-2'>Full-Time</div>
+                <h3 className='text-xl font-bold'>Senior React Developer</h3>
               </div>
 
-              <div>
+              <div className='mb-5'>
                 We are seeking a talented Front-End Developer to join our team in Boston, MA. The ideal candidate will have strong skills in HTML, CSS, and JavaScript...
               </div>
 
-              <h3>$70 - $80K / Year</h3>
+              <h3 className='text-indigo-500 mb-2'>$70 - $80K / Year</h3>
 
-              <div></div>
+              <div className='border border-gray-100 mb-5'></div>
 
-              <div>
-                <div>
-                  <i></i>
+              <div className='flex flex-col lg:flex-row justify-between mb-4'>
+                <div className='text-orange-700 mb-3'>
+                  <i className='fa-solid fa-location-dot text-lg'></i>
                   Boston, MA
                 </div>
-                <a>
+                <a
+                  href="job.html"
+                  className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm">
                   Read More
                 </a>
               </div>
