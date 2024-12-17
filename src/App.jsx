4bde431c15922456fefb782df19de04a5eca6ef3
Amina Wasif <aminawasif20@gmail.com>
2024-12-17 17:05:03 +0500

    Modified App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -108,27 +108,29 @@ const App = () => {
           </div>
 
           {/* Job Listing 2 */}
-          <div>
-            <div>
-              <div>
-                <div>Remote</div>
-                <h3>Front-End Engineer (React)</h3>
+          <div className='bg-white rounded-xl shadow-md relative'>
+            <div className='p-4'>
+              <div className='mb-6'>
+                <div className='text-gray-600 my-2'>Remote</div>
+                <h3  className='text-xl font-bold'>Front-End Engineer (React)</h3>
               </div>
 
-              <div>
+              <div className='mb-5'>
               Join our team as a Front-End Developer in sunny Miami, FL. We are looking for a motivated individual with a passion...
               </div>
 
-              <h3>$70 - $80K / Year</h3>
+              <h3 className='text-indigo-500 mb-2'>$70 - $80K / Year</h3>
 
-              <div></div>
+              <div className='border border-gray-100 mb-5'></div>
 
-              <div>
-                <div>
-                  <i></i>
+              <div className='flex flex-col lg:flex-row justify-between mb-4'>
+                <div className='text-orange-700 mb-3'>
+                  <i className='fa-solid fa-location-dot text-lg'></i>
                   Miami, FL
                 </div>
-                <a>
+                <a
+                  href="job.html"
+                  className='h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm'>
                   Read More
                 </a>
               </div>
@@ -136,27 +138,29 @@ const App = () => {
           </div>
 
           {/* Job Listing 3 */}
-          <div>
-            <div>
-              <div>
-                <div>Remote</div>
-                <h3>React.js Developer</h3>
+          <div className='bg-white rounded-xl shadow-md relative'>
+            <div className='p-4'>
+              <div className='mb-6'>
+                <div className='text-gray-600 my-2'>Remote</div>
+                <h3 className='text-xl font-bold'>React.js Developer</h3>
               </div>
 
-              <div>
+              <div className='mb-5'>
               Are you passionate about front-end development? Join our team in vibrant Brooklyn, NY, and work on exciting projects that make a difference...
               </div>
 
-              <h3>$70 - $80K / Year</h3>
+              <h3 className='text-indigo-500 mb-2'>$70 - $80K / Year</h3>
 
-              <div></div>
+              <div className='border border-gray-100 mb-5'></div>
 
-              <div>
-                <div>
-                  <i></i>
+              <div className='flex flex-col lg:flex-row justify-between mb-4'>
+                <div className='text-orange-700 mb-3'>
+                  <i className='fa-solid fa-location-dot text-lg'></i>
                   Brooklyn, NY
                 </div>
-                <a>
+                <a 
+                  href="job.html"
+                  className='h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm'>
                   Read More
                 </a>
               </div>
