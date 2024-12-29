9f40f2206799a3a118b6e952342617486e150f76
Amina Wasif <77668986+AW-2021@users.noreply.github.com>
2024-12-29 15:10:18 +0500

    Modified HomeCards.jsx and Card.jsx

--- src/components/HomeCards.jsx
+++ src/components/HomeCards.jsx
@@ -1,10 +1,11 @@
+import Card from "./Card";
 
 const HomeCards = () => {
   return (
     <section className='py-4'>
       <div className='container-xl lg:container m-auto'>
         <div className='grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg'>
-          <div className='bg-gray-100 p-6 rounded-lg shadow-md'>
+          <Card>
             <h2 className='text-2xl font-bold'>For Developers</h2>
             <p className='mt-2 mb-4'>Browse our React jobs and start your career today</p>
             <a
@@ -12,9 +13,9 @@ const HomeCards = () => {
               className="inline-block bg-black text-white rounded-lg px-4 py-2 hover:bg-gray-700">
               Browse Jobs
             </a>
-          </div>
+          </Card>
 
-          <div className='bg-indigo-100 p-6 shadow-md rounded-lg'>
+          <Card bg='bg-indigo-100'>
             <h2 className='text-2xl font-bold'>For Employers</h2>
             <p className='mt-2 mb-4'>List your job to find the perfect developer for the role</p>
             <a
@@ -22,7 +23,7 @@ const HomeCards = () => {
               className="inline-block bg-indigo-500 text-white rounded-lg py-2 px-4 hover:bg-indigo-600">
               Add Job
             </a>
-          </div>
+          </Card>
         </div>
       </div>
      </section>
