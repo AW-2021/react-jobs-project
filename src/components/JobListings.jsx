edb830cdf27c3710415cf530d896fe0488d9ddc0
Amina Wasif <aminawasif20@gmail.com>
2025-01-01 11:21:44 +0500

    Created ViewAllJobs.jsx

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -2,7 +2,9 @@ import jobs from '../jobs.json';
 import JobListing from './JobListing';
 
 const JobListings = () => {
-  console.log(jobs)
+  console.log(jobs);
+
+  const recentJobs = jobs.slice(0,3);
 
   return (
     <section className='bg-blue-50 px-4 py-10'>
@@ -10,7 +12,7 @@ const JobListings = () => {
         <h2 className='text-3xl font-bold text-center text-indigo-500 mb-6'>Browse Jobs</h2>
         <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
           {
-            jobs.map((job) => <JobListing job={job} />)
+            recentJobs.map((job) => <JobListing key={ job.id } job={ job } />)
           }
         </div>
       </div>
