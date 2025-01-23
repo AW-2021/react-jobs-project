3ea2a57ec625aed4aad565d51afc293f1e372b5d
Amina Wasif <aminawasif20@gmail.com>
2025-01-23 12:00:08 +0500

    Modified JobListings.jsx and JobsPage.jsx

--- src/pages/JobsPage.jsx
+++ src/pages/JobsPage.jsx
@@ -1,6 +1,13 @@
+import JobListings from "../components/JobListings";
+import jobs from "../jobs.json";
+
 const JobsPage = () => {
+  const totalJobs = jobs.length;
+
   return (
-    <div>Jobs Page</div>
+    <section className='bg-blue-50 px-4 py-6'>
+      <JobListings isHome={false} />
+    </section>
   )
 }
 
