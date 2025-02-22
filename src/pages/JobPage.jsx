c543889974db5636d292b907b0874f724fc9a6e2
Amina Wasif <77668986+AW-2021@users.noreply.github.com>
2025-02-23 00:20:05 +0500

    Fetching data from server with useEffect() in JobPage.jsx

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -1,7 +1,30 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import Spinner from "../components/Spinner";
+import JobListing from "../components/JobListing";
+
 const JobPage = () => {
-  return (
-    <div>JobPage</div>
-  )
+  const { id } = useParams();
+  const [job, setJob] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchJob = async () => {
+       try {
+        const res = await fetch(`/api/jobs/${id}`);
+        const data = await res.json();
+        setJob(data);
+        console.log(job);
+      } catch (error) {
+        console.log("Error fetching data", error);
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchJob();
+  }, []);
+
+  return loading ? <Spinner /> : ( <JobListing job={job} /> );
 }
 
 export default JobPage;
