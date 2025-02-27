f7658e2ea6049324167dfdc1dd7c54e6c189a074
Amina Wasif <aminawasif20@gmail.com>
2025-02-27 14:36:02 +0500

    Added a POST request using Fetch API to App.jsx

--- src/pages/JobsPage.jsx
+++ src/pages/JobsPage.jsx
@@ -1,9 +1,7 @@
 import JobListings from "../components/JobListings";
-import jobs from "../jobs.json";
+// import jobs from "../jobs.json";
 
 const JobsPage = () => {
-  const totalJobs = jobs.length;
-
   return (
     <section className='bg-blue-50 px-4 py-6'>
       <JobListings isHome={false} />
