be3763ffcbdd35c285778a6326c57e861f1989a5
Amina Wasif <aminawasif20@gmail.com>
2025-02-28 16:28:08 +0500

    Created EditJobPage.jsx and added its Route to App.jsx

---
+++ src/pages/EditJobPage.jsx
@@ -0,0 +1,9 @@
+import { useLoaderData } from "react-router-dom";
+
+const EditJobPage = () => {
+  const job = useLoaderData();
+
+  return <div>EditJobPage</div>;
+};
+
+export default EditJobPage;
